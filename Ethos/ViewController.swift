c75b44b3035a0b5ee86cec27b43a75472b0769cd
ccrama <hapticappsdev@gmail.com>
2018-10-06 12:10:56 -0500

    Initial Commit

---
+++ Ethos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ethos
+//
+//  Created by Carlos Crane on 10/6/18.
+//  Copyright © 2018 Crank Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
