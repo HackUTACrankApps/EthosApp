600bf084208da31c34d0d16d7704e01289743432
VijitSingh97 <vijit.singh@mavs.uta.edu>
2018-10-06 14:10:34 -0500

    added hashes to NetworkUtils.swift

--- Ethos/NetworkUtils.swift
+++ Ethos/NetworkUtils.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class NetworkUtils {
     
-    public static var userHash: String {
+    public static var userID: String {
         return UserDefaults.standard.string(forKey: "userHash") ?? ""
     }
     
@@ -29,6 +29,30 @@ class NetworkUtils {
                     
                     let ethosModel = Ethos.init(dictionary: json)
                     completion(ethosModel)
+                } catch let error as NSError {
+                    print(error)
+                    completion(nil)
+                }
+                completion(nil)
+            }
+            }.resume()
+    }
+    
+    static func getHashes(rigID: String,completion: @escaping (_ model: Hashes?) -> ()) {
+        let hashesURL = URL(string: "http://ethosdistro.com/graphs/?rig=" + rigID + "&panel=" + NetworkUtils.userHash + "&type=miner_hashes&json=yes")!
+        URLSession.shared.dataTask(with: hashesURL) { (data, _, error) in
+            if error != nil {
+                print(error ?? "Error loading data")
+                //todo handle this
+                completion(nil)
+            } else {
+                do {
+                    guard let json = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary else {
+                        return
+                    }
+                    
+                    let hashModel = Hashes.init(dictionary: json)
+                    completion(hashModel)
                 } catch let error as NSError {
                     print(error)
                     completion(nil)
