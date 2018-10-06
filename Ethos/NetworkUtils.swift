11ca3f0b0c8c763686c3f34948f8f071d337eaca
VijitSingh97 <vijit.singh@mavs.uta.edu>
2018-10-06 15:48:26 -0500

    added Watts.swift and getWatts to NetworkUtils.swift

--- Ethos/NetworkUtils.swift
+++ Ethos/NetworkUtils.swift
@@ -90,4 +90,28 @@ class NetworkUtils {
             }
             }.resume()
     }
+    
+    static func getWatts(rigID: String,completion: @escaping (_ model: Watts?) -> ()) {
+        let tempsURL = URL(string: "http://ethosdistro.com/graphs/?rig=" + rigID + "&panel=" + NetworkUtils.panelID + "&type=miner_hashes&json=yes")!
+        URLSession.shared.dataTask(with: tempsURL) { (data, _, error) in
+            if error != nil {
+                print(error ?? "Error loading data")
+                //todo handle this
+                completion(nil)
+            } else {
+                do {
+                    guard let json = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary else {
+                        return
+                    }
+                    
+                    let wattsModel = Watts.init(dictionary: json)
+                    completion(wattsModel)
+                } catch let error as NSError {
+                    print(error)
+                    completion(nil)
+                }
+                completion(nil)
+            }
+            }.resume()
+    }
 }
